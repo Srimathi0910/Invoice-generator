fe2c242bee8359023b6da95a69df4cd4b57224c6
Srimathi Nagarajan <srimathinagarajan0910@gmail.com>
2026-01-08 10:55:55 +0000

    Add all new files for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    // Alias for imports
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
-  },
-  // Disable Turbopack by explicitly providing an empty config
-  turbopack: {}, 
+  /* config options here */
 };
 
 export default nextConfig;
