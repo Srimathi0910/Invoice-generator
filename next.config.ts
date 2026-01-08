0ddedafb2849f44d32763667d141f830fb0c702b
Srimathi Nagarajan <srimathinagarajan0910@gmail.com>
2026-01-08 11:28:24 +0000

    Fix Invoice.ts case

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    // Add '@' alias pointing to the root of your project
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
-  },
-  experimental: {
-    turbo: false,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
