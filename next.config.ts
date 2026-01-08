5b7b434c858ba66c63f6bcde58789a2c64476970
Srimathi Nagarajan <srimathinagarajan0910@gmail.com>
2026-01-08 11:05:33 +0000

    Add all new files for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    // Optional: Add alias for imports
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
-  },
-  // Explicitly tell Next.js to use Webpack
-  experimental: {
-    turbo: false,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
