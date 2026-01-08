6204cc43ebfa0a7dbf639f67c9d8f99656e1addc
Srimathi Nagarajan <srimathinagarajan0910@gmail.com>
2026-01-08 11:07:10 +0000

    Add all new files for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Add '@' alias pointing to the root of your project
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+  experimental: {
+    turbo: false,
+  },
 };
 
 export default nextConfig;
