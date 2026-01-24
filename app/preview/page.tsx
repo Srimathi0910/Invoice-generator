6a9aff99d56e1628ad0f43e49f5343b632168857
Srimathi Nagarajan <srimathinagarajan0910@gmail.com>
2026-01-24 16:30:39 +0000

    Add all new files for deployment

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from "react";
 import InvoicePreview from "./InvoicePreview";
 
 export default function PreviewPage() {
-  return (
-    <Suspense fallback={<div className="p-6">Loading preview...</div>}>
-      <InvoicePreview />
-    </Suspense>
-  );
+  return <InvoicePreview />;
 }
